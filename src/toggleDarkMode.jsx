ca43c087ba6828187223716f0b71b9dc42fe465d
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2024-08-06 23:01:37 +0000

    ...

--- src/toggleDarkMode.jsx
+++ src/toggleDarkMode.jsx
@@ -5,6 +5,7 @@ function toggleDarkMode(colors, setColors) {
      r.style.setProperty('--invert', 0);
      r.style.setProperty('--textColor', 'rgba(0,0,0,0.75)');
      r.style.setProperty('--hoverColor', 'rgba(0,0,0,0.2)');
+     r.style.setProperty('--color', 'rgb(196, 196, 196,0.75)');
      setColors(['grey', 'black']);
      
    } else {
@@ -12,6 +13,7 @@ function toggleDarkMode(colors, setColors) {
      r.style.setProperty('--invert', 1);
      r.style.setProperty('--textColor', 'rgba(255,255,255,0.75)');
      r.style.setProperty('--hoverColor', 'rgba(255,255,255,0.2)');
+     r.style.setProperty('--color', 'rgb(48, 48, 48,0.75)');
      setColors(['grey', 'white']);  
    }
 }
