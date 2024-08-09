b931016bdbd74e8d9b28c2366f0fddbc0c066b23
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2024-08-09 22:43:04 +0000

    ...

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0"
-  }
+  base:'/DevEvoTestPage/',
+  plugins: [react()]
+  // server: {
+  //   host: "0.0.0.0"
+  // }
   // base:'/DevEvoTestPage/'
 })
